747aa48f33843c8895487f78ccf0ad1aebaf61f9
null <null>
2023-10-11 12:37:08 -0600

    Agregar archivos de proyecto.

---
+++ Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace biblioUsuarios_G2_2024_1
+{
+    public class Usuario
+    {
+        #region Attributos
+        private string nomUsuario;
+        private string password;
+        #endregion
+
+        #region Propiedades
+        //encapsular
+        public string NomUsuario { get => nomUsuario; set => nomUsuario = value; }  
+        public string Password { get => password; set => password = value; }
+        #endregion
+
+        #region Constructor
+        public Usuario(string nomUsuario, string password)
+        {
+            NomUsuario = nomUsuario;
+            Password = password;
+        }
+        #endregion
+    }
+}
